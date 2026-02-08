2a297231e0f1e10a3d4e863772eb9d6bfd35baf3
shreedharpatel124-ux <shreedharpatel124@gmail.com>
2026-02-08 18:23:11 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  define: {
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+  },
+  build: {
+    target: 'esnext'
+  },
+  server: {
+    port: 3000
+  }
+});
